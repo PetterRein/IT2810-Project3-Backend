8b93b12dfce034fba99a414306623d5b340583c7
Petter Grø Rein <pettegre@stud.ntnu.no>
2019-10-08 12:22:36 +0200

    add code for express #1
    This commit include support for mongoose and nodemon

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 5050;
+app.listen(port, () => console.log(`server running on port ${port}`));
