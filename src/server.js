a1baa03026a7edf96ddc7644709d761d12968d06
Petter Grø Rein <pettegre@stud.ntnu.no>
2019-10-11 17:57:30 +0200

    add endpoint and simple query #11

--- src/server.js
+++ src/server.js
@@ -2,13 +2,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import expressGraphQL from 'express-graphql';
+import schema from './schema'
 const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}))
 
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`server running on port ${port}`));
